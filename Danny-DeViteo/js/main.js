431c272a8bea75312dc5536d6e3f0c63cf25f04d
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2024-01-11 09:17:45 -0500

    ñ

--- Danny-DeViteo/js/main.js
+++ Danny-DeViteo/js/main.js
@@ -1,3 +1,16 @@
 import '/style.css'
 import { DOMSelectors } from './Dom';
 console.log(DOMSelectors);
+
+function search(){
+    DOMSelectors.search.addEventListener("submit", function(event){
+      event.preventDefault();
+      const searchInput = DOMSelectors.input.value;
+          try{
+            console.log(searchInput)
+          }catch(error){
+           
+          }
+        })
+    }
+    search()
