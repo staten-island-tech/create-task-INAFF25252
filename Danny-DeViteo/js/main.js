76c01bc7c7f6e4387fe801917c684e30bf298dba
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2024-01-12 09:22:37 -0500

    ff

--- Danny-DeViteo/js/main.js
+++ Danny-DeViteo/js/main.js
@@ -2,15 +2,19 @@ import '/style.css'
 import { DOMSelectors } from './Dom';
 console.log(DOMSelectors);
 
+const hist = []
 function search(){
     DOMSelectors.search.addEventListener("submit", function(event){
       event.preventDefault();
       const searchInput = DOMSelectors.input.value;
           try{
-            console.log(searchInput)
+            hist.push(searchInput)
+            console.log(hist)
+
           }catch(error){
            
           }
         })
     }
     search()
+    //add if statemnt
