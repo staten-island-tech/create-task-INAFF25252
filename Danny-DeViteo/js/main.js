774603012a5a8e511644974354b99890145a69db
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2024-01-17 09:24:58 -0500

    dw

--- Danny-DeViteo/js/main.js
+++ Danny-DeViteo/js/main.js
@@ -8,13 +8,30 @@ function search(){
       event.preventDefault();
       const searchInput = DOMSelectors.input.value;
           try{
+            
             hist.push(searchInput)
             console.log(hist)
+            const what = searchInput.includes(`"${hist}"`)
+            console.log(what)
+            if(what === true){
+              alert("Oops; already have this gift idea");
+            }
+            if(hist.length > 5){
+              alert("Too poor, can't get more gifts");
+            }
+            clearFields()
+         
+          }
 
-          }catch(error){
+
+          catch(error){
            
           }
         })
+  
     }
     search()
     //add if statemnt
+    function clearFields(){
+      DOMSelectors.input.value = ""
+      }
