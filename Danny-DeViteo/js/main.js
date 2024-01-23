03b734c3a1a89f50761e0ce45983c0085a52b7a8
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2024-01-23 17:38:19 -0500

    1

--- Danny-DeViteo/js/main.js
+++ Danny-DeViteo/js/main.js
@@ -2,46 +2,7 @@ import '/style.css'
 import { DOMSelectors } from './Dom';
 console.log(DOMSelectors);
 
-const url = "https://opentdb.com/api.php?amount=5"
 
-async function getData(URL){
-  try{
-    const response = await fetch(URL)
-    const data = await response.json();
-    console.log(data);
-    return data 
-  } catch(error){
-    console.log(error)    
-  }
-}
-getData(url); 
-
-
-function card(btn){
-  btn.addEventListener("click", function(event){
-    event.preventDefault();
-    //clearButtons()
-    async function otherButtons(){
-      try{
-       const get = await fetch(`${url}`);
-       const poke = await get.json();
-        console.log(poke);
-        poke.results.forEach((item)=>  {
-          DOMSelectors.gallery.insertAdjacentHTML("beforeend", `
-            <div class="card">
-            <h2>${item.question}</h2>
-            </card>`
-          )
-        })  
-      }
-      catch(error){
-
-      }
-    }
-    otherButtons()
-  })
-}
-card(DOMSelectors.form)
 
 const hist = []
 function search(){
